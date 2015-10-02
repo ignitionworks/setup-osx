004376f3972a31de69a2d310584e44b69fe590b1
Theo Cushion <theozaurus@users.noreply.github.com>
2015-10-03 00:10:07 +0100

    Install bash-it with git

--- setup.sh
+++ setup.sh
@@ -20,4 +20,7 @@ brew install chruby
 
 # Install bash-it
 echo "Installing bash-it..."
-brew install bash-it
+if [ ! -d ~/.bash_it ]; then
+  git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+  ~/.bash_it/install.sh
+fi
