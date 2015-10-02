e603a6da0174d33516fed94bb3bea3b678961d74
Theo Cushion <theozaurus@users.noreply.github.com>
2015-10-03 00:11:44 +0100

    Add updating bash_it

--- setup.sh
+++ setup.sh
@@ -19,8 +19,13 @@ echo "Installing chruby..."
 brew install chruby
 
 # Install bash-it
-echo "Installing bash-it..."
 if [ ! -d ~/.bash_it ]; then
+  echo "Installing bash-it..."
   git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
   ~/.bash_it/install.sh
+else
+  echo "Updating bash-it..."
+  pushd ~/.bash_it
+  git pull
+  popd
 fi
