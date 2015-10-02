1af5621176a30de8bc64eeb5b2284b72bcc4093c
Theo Cushion <theozaurus@users.noreply.github.com>
2015-10-02 11:24:14 +0100

    Switch to sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
-#!/bin/bash -xe
+#!/usr/bin/env sh
 
 # Install homebrew
-hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" }
+if ! command -v brew > /dev/null 2>&1; then
+  echo "Okay, lets do this. First things first, we need to install homebrew in order to get all the stuff you need"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+  echo "Homebrew was already available... skipping the installation"
+  echo "Let's update it instead! (you know you haven't updated in a while)"
+  brew update
+fi
 
 # Install Brew Cask
 brew install caskroom/cask/brew-cask
