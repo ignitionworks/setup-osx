946762fd08ad7b943170ae06f1e129cc694d692b
Theo Cushion <theozaurus@users.noreply.github.com>
2015-10-02 11:16:29 +0100

    Initial commit

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install homebrew
+hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" }
+
+# Install Brew Cask
+brew install caskroom/cask/brew-cask
+
+# Install chruby
+brew install chruby
+
+# Install bash-it
+brew install bash-it
