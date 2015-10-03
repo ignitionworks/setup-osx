c969ee5abdb213d88df33b9a19ba95664c3891d7
Theo Cushion <theozaurus@users.noreply.github.com>
2015-10-03 10:36:22 +0100

    Add sensible OS X defaults

--- setup.sh
+++ setup.sh
@@ -13,6 +13,11 @@ fi
 # Install Homebrew Cask
 echo "Installing Homebrew Cask..."
 brew install caskroom/cask/brew-cask
+brew cask search
+brew cask install google-chrome
+brew cask install skype
+brew cask install slack
+brew cask install atom
 
 # Install chruby
 echo "Installing chruby..."
@@ -29,3 +34,33 @@ else
   git pull
   popd
 fi
+
+# Setup osx defaults
+
+# Show icons for servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Prevent Time Machine from prompting to use new hard drives as backup volume
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+# Setup terminal to use Homebrew
+defaults write ~/Library/Preferences/com.apple.Terminal.plist "Default Window Settings" "Homebrew"
+defaults write ~/Library/Preferences/com.apple.Terminal.plist "Startup Window Settings" "Homebrew"
