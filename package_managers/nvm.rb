1a5f703fef95445f197ed5ea6f4e55e3ede24d2f
Theo Cushion <theo@ignition.works>
2015-10-12 20:13:02 +0100

    Switch to normal version of NVM, not bash-it version

--- package_managers/nvm.rb
+++ package_managers/nvm.rb
@@ -1,4 +1,11 @@
 desc 'Install nvm'
 task 'package_managers:nvm' => 'settings:bash_it' do
-  system('bash -lc "bash-it enable plugin nvm"')
+  nvm_path = File.expand_path('~/.nvm')
+  system("git clone https://github.com/creationix/nvm.git #{nvm_path}") unless File.exist?(nvm_path)
+  Dir.chdir(nvm_path) do
+    system('git checkout `git describe --abbrev=0 --tags`')
+  end
+  File.open(File.expand_path('~/.bash_it/custom/nvm.bash'), 'w') do |f|
+    f.write("source #{nvm_path}/nvm.sh")
+  end
 end
